cd94a5d6eac987ca4c00e0b0baa9dc98a0072ccb
Aleh Barushka <1.alehbarushka@gmail.com>
2023-06-15 23:45:09 +0300

    feat: :bento: add bootsplash screen

--- app/App.tsx
+++ app/App.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useRef} from 'react';
 import {AppState} from 'react-native';
 import {Provider} from 'react-redux';
+import RNBootSplash from 'react-native-bootsplash';
 
 import Navigation from './navigation';
 import {store} from './store';
@@ -13,6 +14,8 @@ const App: React.FC = () => {
       appState.current = nextAppState;
       console.log('AppState', appState.current);
     });
+
+    RNBootSplash.hide({fade: true, duration: 500});
 
     return () => {
       subscription.remove();
